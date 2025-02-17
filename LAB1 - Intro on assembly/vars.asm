96614d995d748392ae6757f37b98d97c50347efe
abanoub-refaat <abanoubref3at18@gmail.com>
2025-02-17 20:46:25 +0200

    Arrays and vars in Assembly

--- LAB1 - Intro on assembly/vars.asm
+++ LAB1 - Intro on assembly/vars.asm
@@ -2,7 +2,16 @@
 mov ah, var1
 mov dl, var2
 xchg ah, dl
+mov bh, a[0]
+mov bl, a[1]
+mov dh, a[4]  
+mov cl, b[8]
+; We can define vars befor the `ret` keyword using the `EQU` keyword
+c EQU 6
+mov ah, c
 ret
 ; defining the vars we want.
 var1 DB 12H
 var2 DB 59H
+a DB 12H,34H,11H,48H,98H ;An array of length 5. 
+b DB 10 DUP (95H) ;An array of length 10 all element are 95H.
