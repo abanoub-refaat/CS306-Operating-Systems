831c93bd214ee02bbc8d80f1eed0465eeee4d51b
abanoub-refaat <abanoubref3at18@gmail.com>
2025-03-25 16:20:48 +0200

    Add multithreading task in Lab 6 with basic thread creation and struct usage

---
+++ Labs/Lab6_CandMultithreding/task.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+
+struct pair
+{
+    int x;
+    int y;
+} p1;
+
+void *method(void *args)
+{
+    struct pair *p1 = (struct pair *)args;
+    printf("Here in the method x = %d, and y = %d\n", (*p1).x, (*p1).y);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t thread;
+
+    p1.x = 3;
+    p1.y = 5;
+
+    if (pthread_create(&thread, NULL, method, &p1) != 0)
+    {
+        printf("Error\n");
+        return 1;
+    }
+    return 0;
+}
